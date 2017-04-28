41b1fd1c10468499f10123f4595d2d18a33c467f
SUMAN CHATTERJEE <sumanismcse@gmail.com>
2017-04-28 21:13:15 +0530

    Add files via upload

---
+++ suman (2).php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My first php web page after a fucking long time </title>
+</head>
+<body>
+<?php
+print('hello fucking world!');
+echo "hello world"; 
+$color= "red";
+echo " and my fucking car is " . $color . "<br>";
+?>
+</body>
+</html>
---
+++ suman.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My first php web page after a fucking long time </title>
+</head>
+<body><?php print('hello fucking world!');
+echo "hello world"; ?>
+</body>
+</html>
